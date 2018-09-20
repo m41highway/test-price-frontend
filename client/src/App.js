47e512127be18ff2726ff3e120750473bc6d3fbb
m41highway@gamil.com <thaib@Macintosh.local>
2018-09-21 07:31:26 +0800

    Add react frontend with css

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Moment from 'moment'
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    average: 0,
+    prices: []
+  }
+  
+  fetchData() {
+    fetch('http://ec2-18-191-186-71.us-east-2.compute.amazonaws.com:5000/latestAveragePrice')
+    .then(response => response.json())  
+    .then(res => {
+      this.setState({ average: res.message.average })
+      this.setState({ prices: res.message.data })
+    });
+  }
+
+  handleClick(e) {
+    // this.saySomething("element clicked");
+    this.fetchData()
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Header">
+          The current average price is <span className="Average-price">${ Number(this.state.average).toFixed(2) }</span>
+          <input className="Refresh-button" type="button" value="Refresh" onClick = {this.handleClick.bind(this)} />
+        </div>
+        
+        <div className="Price-list">
+          {this.state.prices.map( priceItem =>
+            <div className="Price-row">${Number(priceItem.price).toFixed(2)} - {Moment(priceItem.timestamp).format("YYYY-MM-DD hh:mm:ss")}</div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
